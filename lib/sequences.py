c6992129c76f159e6d213514c6856e1ccd05d2c1
steve passarelli <passarellism@gmail.com>
2023-02-18 13:46:39 -0700

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
+
+
 def print_fibonacci(length):
-    pass
+    my_list = []
+    for n in range(length):
+        if n == 0 or n == 1:
+            (my_list.append(n))
+        else:
+            my_list.append(my_list[(n-1)] + my_list[(n-2)])
+    print (my_list)
+
